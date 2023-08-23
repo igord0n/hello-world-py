26e58fa1ce908e9bdd4f50b4ccc2cf8c8123420d
Igor <dummerigor@gmail.com>
2023-08-23 17:11:49 -0300

    first-hello-world
    First hello world in python using an input to asking name to greeting.

---
+++ hello-world.py
@@ -0,0 +1,5 @@
+name = str(input("Could you say your name? "))
+print("Hello, world! By the way, how are you {}?".format(name))
+print("This is my first Hello World in python, {}. Did I welldone? ^.^".format(name))
+
+# This is my first hello world in Python to commit at GitHub.
